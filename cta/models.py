d8ffde71c9ec449d2ee56683ef09b3ba80a7a3a7
Jack Carter <carter.jack@gmail.com>
2014-01-18 13:36:49 -0600

    Center map on a Chicago stop, add CSS

--- cta/models.py
+++ cta/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 from django.utils import timezone
 
-
-class Stop(models.Model):
-    id = models.IntegerField(unique=True)
-
-    def __unicode__(self):
-        return self.title
