78e822f9aa01f49deefd5f93b81f09868323d872
Jack Carter <carter.jack@gmail.com>
2014-01-17 23:03:20 -0600

    Begin to develop function to get date and parse it out of XML

--- ctatracker.py
+++ ctatracker.py
@@ -1,9 +1,14 @@
 import requests
 import keys
+import datetime
+import xml.etree.ElementTree as ET
 
-base_url = "http://www.ctabustracker.com/bustime/api/v1/gettime"
-params = {"key":keys.cta}
-
-r = requests.get(base_url, params=params)
-
-print r.text
+base_url = "http://www.ctabustracker.com/bustime/api/v1/"
+	
+def get_time():
+	params = {"key":keys.cta}
+	request_string = "gettime"
+	r = requests.get(base_url + request_string, params=params)
+	root = ET.fromstring(r.text)
+	print root.find('tm').text
+get_time()
