0dcfb057de9ffac56a064d354e7b137570057a20
Jack Carter <carter.jack@gmail.com>
2014-01-19 16:53:02 -0600

    Untrack some db files

--- cta/cacherouteinfo.py
+++ cta/cacherouteinfo.py
@@ -9,9 +9,16 @@ def save_routes():
 		route_obj = Route(route_id=route['route'], route_name=route['route_name'])
 		route_obj.save()
 		directions = a.get_directions(route['route'])
+		
 		for direction in directions:
 			direction_obj, direction_created = Direction.objects.get_or_create(direction=direction)
+
+			if direction_created:	
+				direction_obj.save()				
+			route_obj.directions.add(direction_obj)
+			
 			stops = a.get_stops(route['route'], direction)
+
 			for stop in stops:
 				stop_obj, stop_created = Stop.objects.get_or_create(
 					stop_id=stop['stop_id'],
@@ -21,8 +28,6 @@ def save_routes():
 				)
 				if stop_created:
 					stop_obj.save()
-				route_obj.stops.add(stop_obj)
-			if direction_created:	
-				direction_obj.save()				
-			route_obj.directions.add(direction_obj)		
-		route_obj.save()
+				route_obj.stops.add(stop_obj)	
+
+#		route_obj.save()
