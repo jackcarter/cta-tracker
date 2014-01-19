50735bebfbf5ab7f0f2128da35b9e204409c72df
Jack Carter <carter.jack@gmail.com>
2014-01-18 22:50:35 -0600

    Use ajax to get all stops on route 72 Eastbound, then plot them as markers

--- cta/ajax.py
+++ cta/ajax.py
@@ -1,6 +1,8 @@
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
+import ctatracker
 
+a = ctatracker.BusTracker()
 @dajaxice_register
 def sayhello(request):
-    return simplejson.dumps({'message':'Hello World'})
+    return simplejson.dumps(a.get_stops('72','Eastbound'))
--- cta/static/cta/map.js
+++ cta/static/cta/map.js
@@ -1,8 +1,26 @@
+var map;
 function initialize() {
 	var mapDiv = document.getElementById('map_canvas');
-	var map = new google.maps.Map(mapDiv, {
+	map = new google.maps.Map(mapDiv, {
 		center: new google.maps.LatLng(41.910277822061, -87.686948776245),
 		zoom: 12,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	});
+}
+function addStops(data) {
+  for (var object in data) {
+    var latLng = new google.maps.LatLng(data[object].latitude, data[object].longitude);
+	console.log(data[object]);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  }
+}
+function addStop(latitude, longitude) {
+    var latLng = new google.maps.LatLng(latitude, longitude);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
 }
