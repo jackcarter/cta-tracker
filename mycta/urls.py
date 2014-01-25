5cd3dbc7199763a9411a883dbc2f7cd1e794c92e
Jack Carter <carter.jack@gmail.com>
2014-01-25 16:56:59 -0600

    Redirect base url to /cta/

--- mycta/urls.py
+++ mycta/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from django.views.generic import RedirectView
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,13 +11,10 @@ from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 dajaxice_autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mycta.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
 	url(r'^cta/', include('cta.urls', namespace='cta')),
 	url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
+	url(r'^$', RedirectView.as_view(url='/cta/')),
 )
 
 urlpatterns += staticfiles_urlpatterns()
